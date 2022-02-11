37ce96678b1109c997820eb2900fc5a7e797ce6b
I-the-Creator <andrey.burluckiy@gmail.com>
2022-02-11 17:02:56 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+const  start = () => {
+    try {
+        
+    } catch (e) {
+        console.log(e);
+    }
+}
