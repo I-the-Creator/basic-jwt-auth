7ee928c599ef9101062938ab9a8df69d51ed07ce
I-the-Creator <andrey.burluckiy@gmail.com>
2022-02-13 16:33:21 +0100

    'User' schema created

--- models/User.js
+++ models/User.js
@@ -1,1 +1,13 @@
 // Scheme of 'User' entity
+
+const { Schema, model } = require('mongoose')
+
+//Schema - как пользователь хранится в БД
+const User = new Schema({
+    username: {type: String, inique: true, required: true},
+    password: {type: String, required: true},
+    roles: [{type: String, ref: 'Role'}]  // набор ролей пользователя 
+})
+
+//Model
+module.exports = model('User', User);  // model name and schema as a paremeters
